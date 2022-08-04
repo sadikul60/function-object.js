be716c9ea0057b800ec59e9ffa9bd13abe004792
mdsadikul <mdsadikulislam891@gamil.com>
2022-08-04 09:08:24 +0600

    added Object

---
+++ object.js
@@ -0,0 +1,26 @@
+
+/* object Student */
+var student = {
+    roll: 116491,
+    name: 'Md. Sadikul Islam',
+    class: 11,
+    shift: '2nd',
+    technology: 'Computer'
+}
+console.log(student);
+console.log(student.roll);
+console.log(student.name);
+
+
+/* object mobile */
+var mobile = {
+    Brand: 'Redme Note 4',
+    Price: 10000,
+    RAM: '4GB',
+}
+
+// object value replace and object value add
+console.log(mobile.brand);
+mobile.ROM = '64GB';
+mobile.Brand = 'Sumsang j7'
+console.log(mobile)
